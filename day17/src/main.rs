aea61c9797bc168e6c77dbfdadebb1005ebc995e
Adam Fortune <adamfortune96@protonmail.com>
2023-12-01 22:08:34 -0600

    Set up template for the other 24 days

---
+++ day17/src/main.rs
@@ -0,0 +1,15 @@
+use std::fmt::Display;
+
+fn main() {
+    let data = include_str!("../data.txt");
+    println!("part 1: {}", part1(data));
+    println!("part 2: {}", part2(data));
+}
+
+fn part1(data: &str) -> impl Display {
+    ""
+}
+
+fn part2(data: &str) -> impl Display {
+    ""
+}
